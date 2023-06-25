03dc9aec9227da67c13d85902f2f3e7eaaed48d9
Sonu Munda <79930169+SonuMunda@users.noreply.github.com>
2023-06-25 14:12:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,11 +38,11 @@ function App() {
           <Header openWidget={openWidget} />
           <Routes>
             <Route path="/" element={<Home openWidget={openWidget} />} />
-            <Route path="https://nashtoclub.netlify.app//timings" element={<Timings />} />
-            <Route path="https://nashtoclub.netlify.app//timings/menu" element={<Menu />} />
-            <Route path="https://nashtoclub.netlify.app//timings/story" element={<Story />} />
-            <Route path="https://nashtoclub.netlify.app//timings/order" element={<Order />} />
-            <Route path="https://nashtoclub.netlify.app//timings/contact" element={<Contact />} />
+            <Route path="https://nashtoclub.netlify.app/timings" element={<Timings />} />
+            <Route path="https://nashtoclub.netlify.app/menu" element={<Menu />} />
+            <Route path="https://nashtoclub.netlify.app/story" element={<Story />} />
+            <Route path="https://nashtoclub.netlify.app/order" element={<Order />} />
+            <Route path="https://nashtoclub.netlify.app/contact" element={<Contact />} />
             <Route path="*" element={<ErrorPage />} />
           </Routes>
           <Reservation />
