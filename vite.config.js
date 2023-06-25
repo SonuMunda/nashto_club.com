9b372d7856a87b944a459e3f1c53113dfa1e2503
Sonu Munda <sonumunda1312@gmail.com>
2023-06-25 14:45:27 +0530

    Things modified

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+const history = require("connect-history-api-fallback");
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -6,4 +7,14 @@ export default defineConfig({
   plugins: [react()],
 });
 
-
+module.exports = {
+  server: {
+    middleware: {
+      // Add the history API fallback middleware
+      // to redirect all requests to the index.html file
+      before: function(app, server, compiler) {
+        app.use(history());
+      },
+    },
+  },
+};
